6ef256e95db60b3b0cba5f3bd02192eefe4dd383
Jason Montleon <jmontleo@redhat.com>
2021-06-29 08:13:55 -0400

    create fifo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-ncat -k -l 25 < smtp_to_bridge | ncat 127.0.0.1 1025 > smtp_to_bridge &
+mkfifo /tmp/smtp_to_bridge
+ncat -k -l 25 < /tmp/smtp_to_bridge | ncat 127.0.0.1 1025 > /tmp/smtp_to_bridge &
 mkfifo /tmp/fifo
 cat /tmp/fifo | protonmail-bridge --cli
