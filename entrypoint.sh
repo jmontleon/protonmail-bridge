c5557dac6918a05e946764c285aff1d335ddceea
Jason Montleon <jmontleo@redhat.com>
2021-06-29 07:49:43 -0400

    another try

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025,end-close &
+socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
 mkfifo /tmp/fifo
-cat /tmp/fifo | protonmail-bridge --cli
+{ echo info ; cat /tmp/fifo ; } | protonmail-bridge --cli
