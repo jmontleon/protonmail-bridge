07e5d7cfa841d9faac288022b95977d7cf746cf7
Jason Montleon <jmontleo@redhat.com>
2021-06-29 08:26:14 -0400

    uhhh... why is everyone using --cli

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
+protonmail-bridge --noninteractive
 mkfifo /tmp/smtp_to_bridge
-ncat -k -l 25 < /tmp/smtp_to_bridge | ncat 127.0.0.1 1025 > /tmp/smtp_to_bridge &
-mkfifo /tmp/fifo
-cat /tmp/fifo | protonmail-bridge --cli
+ncat -k -l 25 < /tmp/smtp_to_bridge | ncat 127.0.0.1 1025 > /tmp/smtp_to_bridge
