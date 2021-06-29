293971d41ea83ae2bb5dfd04b01a2a429dbc41ab
Jason Montleon <jmontleo@redhat.com>
2021-06-29 07:37:33 -0400

    another try

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -eufo pipefail
-
-socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
+socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025,end-close &
 mkfifo /tmp/fifo
 cat /tmp/fifo | protonmail-bridge --cli
