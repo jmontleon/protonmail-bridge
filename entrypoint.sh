ed7512c6644467db9e4834981e63b389528bb702
Jason Montleon <jmontleo@redhat.com>
2021-06-29 07:26:00 -0400

    try something else. add action to build on push

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-socat -t 5 TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
+set -eufo pipefail
+
+socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
 mkfifo /tmp/fifo
 cat /tmp/fifo | protonmail-bridge --cli
